c0178120ff5554fcc96defd813708dd5699cd179
Bagus TRIANURDIN <btstargroup@gmail.com>
2021-07-07 23:52:43 +0700

    Activate swRegister & change cache name

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: "anz-"+Date.now(),
+  CACHE_NAME: "anzun-resto",
   DATABASE_NAME: 'restaurant_fav',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
